118234aa470edab134dac3437ab3d6863410ee3f
Mostafa Alaa Mohamed <59115691+MostafaFarwiz@users.noreply.github.com>
2023-02-07 02:08:42 +0200

    Advanced New

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+#include <elf.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 /**
  * _strncmp - compare two strings
  * @s1: the first string
